3b130bfa34dd0b2879a211a2def8ed3e44e697c6
ezejichinemerem <ezejichinemerem34@gmail.com>
2026-07-14 15:42:59 +0300

    fixed my image issues

--- front-end/src/Components/FoodItem/FoodItem.jsx
+++ front-end/src/Components/FoodItem/FoodItem.jsx
@@ -4,12 +4,12 @@ import { assets } from "../../assets/assets";
 import { StoreContext } from "../../Context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+  const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img className="food-item-image" src={"http://localhost:4000/images/" + image} alt={name} />
+        <img className="food-item-image" src={url + "/image/" + image} alt={name} />
 
         {!cartItems?.[id] ? (
           <img className="add" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
