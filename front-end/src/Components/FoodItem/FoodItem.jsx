ce8454427de22862cfb5a6b85405110e9791a255
ezejichinemerem <ezejichinemerem34@gmail.com>
2026-07-14 19:24:51 +0300

    fixed some frontend issues

--- front-end/src/Components/FoodItem/FoodItem.jsx
+++ front-end/src/Components/FoodItem/FoodItem.jsx
@@ -9,7 +9,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img className="food-item-image" src={url + "/image/" + image} alt={name} />
+        <img className="food-item-image" src={url + "/images/" + image} alt={name} />
 
         {!cartItems?.[id] ? (
           <img className="add" onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
